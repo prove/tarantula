f635cafa8a1a121dadcdfa61f7a78bcc2382883a
ander <antti.hakala@gmail.com>
2012-06-25 15:10:56 +0300

    add swf to asset paths

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,9 @@ module Tarantula
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    
+    config.assets.paths << Rails.root.join("app", "assets", "swf")
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
