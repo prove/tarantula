5565a3e99c75d3b2e52e861c75fd2d3f530a8a5d
Juhamatti Niemelä <juhamatti.niemela@prove.fi>
2012-06-27 09:02:44 +0300

    Fix layout issues at the login page
    - Allow accessing /assets without logging in

--- lib/authenticator.rb
+++ lib/authenticator.rb
@@ -4,20 +4,20 @@ A rack middleware for handling authentication.
 
 =end
 class Authenticator
-  
-  UnAuthenticated = /^(\/home\/login|\/home\/logout|\/password_resets.*)$/
+
+  UnAuthenticated = /^(\/home\/login|\/home\/logout|\/password_resets.*|\/assets.*)$/
   ######
   class SessionAuth < Rack::Auth::AbstractHandler
-    
+
     def initialize(app, realm=nil, &authenticator)
       @app = app
     end
-    
+
     def call(env)
       return unauthorized unless User.find_by_id(env['rack.session'][:user_id])
       @app.call(env)
     end
-    
+
     private
     def unauthorized
       [ 302, {'Content-type' => 'text/plain', 'Location' => '/home/login',
@@ -25,18 +25,18 @@ class Authenticator
     end
   end
   #####
-  
-  
+
+
   def initialize(app, realm=nil)
     @app = app
     @realm = realm
     @auths = [SessionAuth.new(app, realm){}]
   end
-  
+
   def select_auth(env)
     @auths.first
   end
-  
+
   def call(env)
     if env["PATH_INFO"] =~ UnAuthenticated
       @app.call(env)
