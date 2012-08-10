d8dc08bef0db116f8f95b6f1cc16567cbe16f800
Juhamatti Niemelä <juhamatti.niemela@prove.fi>
2012-08-10 11:04:50 +0300

    Fix issues with missing email content #14
    - ActionMailer of the current Rails version didn't render content from
    old *.rhtml templates anymore
    - Plain text mail templates were renamed to *.text.erb
    - Removed unused obsolete activation.rhtml template

--- app/models/observers/user_observer.rb
+++ app/models/observers/user_observer.rb
@@ -7,8 +7,4 @@ class UserObserver < ActiveRecord::Observer
   def after_create(user)
     UserNotifier.signup_notification(user).deliver if Rails.env != 'test'
   end
-
-  def after_save(user)
-    #UserNotifier.deliver_activation(user) if user.recently_activated?
-  end
 end
