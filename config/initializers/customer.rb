1f813a7c9b870c2af39d0f1bb30cc04159874e91
Juhamatti Niemelä <juhamatti.niemela@prove.fi>
2012-08-14 09:34:38 +0300

    Rake task for noninteractive config usable from scripts

--- config/initializers/customer.rb
+++ config/initializers/customer.rb
@@ -8,25 +8,26 @@ module Testia
   # SERVER ROOT URL.
   # This is used in notifications sent by server to new user
   # accounts (where application is available to users).
-  port = CustomerConfig.port.blank? ? '' : ":#{CustomerConfig.port}" 
+  port = CustomerConfig.port.blank? ? '' : ":#{CustomerConfig.port}"
   WWW_SERVER="#{CustomerConfig.protocol}://#{CustomerConfig.host}#{port}"
-  
+
   # WWW server directory from where Testia application is available.
   # If application is installed to www root, this is empty string.
   # If application is available from subdir, specify directory with
   # leading slash. E.g. WWW_PATH = '/testia'
   #
-  # Used by client to address requests correctly. 
+  # Used by client to address requests correctly.
   #
   WWW_PATH= ''
 
-  # Tool administrators email address.
-  # Is used by automatic notifications
-  ADMIN_EMAIL = CustomerConfig.admin_email
-  
+  # Set email address which is used as from address for automatic
+  # notifications.  Try to use notification_email setting from
+  # customer_configs table and fallback to admin_email setting.
+  ADMIN_EMAIL = CustomerConfig.notification_email || CustomerConfig.admin_email
+
   # How many items to load at once at tagged lists.
   # No need to change this.
   LOAD_LIMIT = 500
-  
+
   DEFAULT_REPORT_CACHE_TIME = 1.minute
 end
--- lib/tasks/db_config.rake
+++ lib/tasks/db_config.rake
@@ -14,5 +14,22 @@ namespace :db do
       puts "\nDone."
     end
 
+    desc "Setup configuration non-interactively from environment vars"
+    task :fromenv => [:environment] do
+      CustomerConfig.protocol = ENV['PROTO'] || 'https'
+      CustomerConfig.host = ENV['HOST']
+
+      # This email will be used as Admin user's mail address
+      CustomerConfig.admin_email = ENV['EMAIL']
+      # This will be from address in automatic notifications
+      CusotmerConfig.notification_email = ENV['NOTIFEMAIL']
+
+      CustomerConfig.smtp = {
+        :address => 'localhost',
+        :port => '25',
+        :domain => ENV['MAILDOMAIN']
+      }
+    end
+
   end
 end
