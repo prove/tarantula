d71a0573740e0a83d36f89c00150eee48670d8ed
ekhatko <ekhatko@at-consulting.ru>
2012-10-07 12:42:37 +0400

    datepicker fix for TestObject model (as example)

--- public/assets/ext/examples/layout/feed-proxy.php
+++
@@ -1,9 +0,0 @@
-<?php
-// this is an example server-side proxy to load feeds
-$feed = $_REQUEST['feed'];
-if($feed != '' && strpos($feed, 'http') === 0){
-	header('Content-Type: text/xml');
-	readfile($feed);
-	return;
-}
-?>
