8b2d839fe942d2be7cf3785dccc76df3f5cd37fc
Juhamatti Niemelä <iiska@iki.fi>
2014-06-24 20:03:41 +0300

    Fix shared examples for Auditable

--- spec/support/auditable.rb
+++ spec/support/auditable.rb
@@ -2,22 +2,22 @@
 
 shared_examples_for "auditable" do
   describe "#change_history" do
-    
+
     it "should return created as first history info" do
       user = User.make!
-      i = get_instance(:updated_by => user.id)
+      i = get_instance(:updater => user)
       hist = i.change_history
       hist.size.should == 1
       hist.first[:comment].should == 'created'
     end
-    
+
     it "should return 'unknown' for user where change has no user" do
       user = User.make!
-      i = get_instance
+      i = get_instance(:updater => nil)
       hist = i.change_history
       hist.first[:user].should == 'unknown'
     end
-    
+
     it "should return user, time, and comment for a single change" do
       user = User.make!
       i = get_instance(:updated_by => user.id)
@@ -32,7 +32,7 @@ shared_examples_for "auditable" do
       change[:user].should == user.login
       change[:time].should be_kind_of(Time)
     end
-    
+
     it "should return multiple changes if present" do
       user = User.make!
       bob = User.make!(:login => 'bob fleming')
@@ -40,21 +40,20 @@ shared_examples_for "auditable" do
       i.change_comment = 'a comment'
       i.updater = user
       i.save!
-      
+
       i.change_comment = 'tasty birds'
       i.updater = bob
       i.save!
-      
+
       hist = i.change_history
       hist.size.should == 3
-      
+
       hist[0][:comment].should == 'tasty birds'
       hist[0][:user].should == 'bob fleming'
-      
+
       hist[1][:comment].should == 'a comment'
-      hist[1][:user].should == user.login      
-    end    
-    
+      hist[1][:user].should == user.login
+    end
+
   end
 end
-
