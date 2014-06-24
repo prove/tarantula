55e356defc78683d2d33627ba6dc451fc9ed47a8
Juhamatti Niemelä <iiska@iki.fi>
2014-06-24 20:04:31 +0300

    Create tmp dir before backup if it won't exist

--- lib/backup.rb
+++ lib/backup.rb
@@ -1,24 +1,27 @@
 =begin rdoc
 
-Backup does a zip backup of database and attachments. 
+Backup does a zip backup of database and attachments.
 
 =end
 class Backup
   BACKUP_ZIP = File.join(Rails.root, 'tmp', 'backup.zip')
 
   def process
+    tmp_dir = File.join(Rails.root, 'tmp')
+    Dir.mkdir(tmp_dir) unless File.exists?(tmp_dir)
+
     attachment_files = Dir.glob(File.join(Rails.root, 'attachment_files', '*'))
-    db_backup = File.join(Rails.root, 'tmp', 'database_backup.sql')
+    db_backup = File.join(tmp_dir, 'database_backup.sql')
     if db_conf['password'].blank?
       passwd = ''
     else
       passwd = "-p #{db_conf['password']}"
     end
-    
+
     system "mysqldump #{db_conf['database']} -u #{db_conf['username']} #{passwd} > #{db_backup}"
-    
+
     FileUtils.rm_f BACKUP_ZIP
-    
+
     Zip::ZipFile.open(BACKUP_ZIP, Zip::ZipFile::CREATE) do |zipfile|
       attachment_files.each do |filename|
         zipfile.add(File.basename(filename), filename)
@@ -26,9 +29,9 @@ class Backup
       zipfile.add(File.basename(db_backup), db_backup)
     end
   end
-  
+
   private
-  
+
   def db_conf
     Rails.configuration.database_configuration[Rails.env]
   end
